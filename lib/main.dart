8edc7124d1ae4f5d6749a753e4c21b7bce1a70b7
Udith <udithshalinda2@gmail.com>
2019-05-10 19:52:08 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import './ui/welcome_home.dart';
+
+void main() {
+  runApp( new MaterialApp(
+      title:'welcome',
+      home: new MyApp(),
+  )
+  );
+}
+
---
+++ lib/ui/welcome_home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        color: Colors.blue,
+        alignment: Alignment.center,
+
+        child: new Text(
+            "Hello there!",
+            textDirection: TextDirection.ltr,
+            style: new TextStyle(
+              fontSize: 34,
+              fontWeight: FontWeight.w800,
+              fontStyle: FontStyle.italic,
+              color: Colors.red,
+            ),
+        ),
+
+    );
+  }
+}
