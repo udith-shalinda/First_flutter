6de0f9e0711f3092cdaaef5c6c7acbf5e4228467
Udith <udithshalinda2@gmail.com>
2019-05-12 19:47:34 +0530

    set State

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 import './ui/welcome_home.dart';
 import './ui/toolbar.dart';
+import './ui/makeItRain.dart';
 
 void main() {
   runApp( new MaterialApp(
       title:'welcome',
-      home: new toolbar(),
+      home: new MakeItRain(),
   )
   );
 }
