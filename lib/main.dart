98b358e55ecf6d896273809e6fd833131c6a2298
Udith <udithshalinda2@gmail.com>
2019-05-13 00:17:24 +0530

    simple login

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 import './ui/welcome_home.dart';
 import './ui/toolbar.dart';
 import './ui/makeItRain.dart';
+import './ui/login.dart';
 
 void main() {
   runApp( new MaterialApp(
       title:'welcome',
-      home: new MakeItRain(),
+      home: new Login(),
   )
   );
 }
